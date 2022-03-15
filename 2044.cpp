b3c5c7f643fa4d77e1275b611f53b5480f6f9a47
男孩 燃烧的 <xdy19981010@outlook.com>
2022-03-15 19:37:55 +0800

    2044. 统计按位或能得到最大值的子集数目

---
+++ 2044.cpp
@@ -0,0 +1,38 @@
+// 2044. 统计按位或能得到最大值的子集数目
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<int>nums{ 3,1 };
+
+    int n = 1 << nums.size(), cnt = 0, maxscore = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if ((i >> j) & 1)
+            {
+                temp |= nums[j];
+            }
+        }
+        if (temp == maxscore)
+        {
+            cnt++;
+        }
+        if (temp > maxscore)
+        {
+            cnt = 1;
+            maxscore = temp;
+        }
+    }
+    return cnt;
+
+    system("pause");
+    return 0;
+}
