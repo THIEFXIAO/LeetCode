617c8d7aaf4bb55146b6a380bb6baebf84310e4a
男孩 燃烧的 <xdy19981010@outlook.com>
2022-04-01 00:08:53 +0800

    20. 有效的括号

---
+++ 20.cpp
@@ -0,0 +1,26 @@
+// 20. 有效的括号
+
+#include<iostream>
+#include<string>
+#include<stack>
+
+int main() {
+
+	std::string s = "(])";
+
+	std::stack<char>brackets;
+	for (size_t i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '(' || s[i] == '[' || s[i] == '{') brackets.push(s[i]);
+		else if (!brackets.empty())
+		{
+			if (s[i] == ')' && brackets.top() == '(') brackets.pop();
+			else if (s[i] == ']' && brackets.top() == '[') brackets.pop();
+			else if (s[i] == '}' && brackets.top() == '{') brackets.pop();
+			else return 0;
+		}
+		else return 0;//0表示flase，1表示true
+	}
+	if (brackets.empty()) return 1;
+	return 0;
+}
